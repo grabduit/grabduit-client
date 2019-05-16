b770b41df0f49361484c8ca66c0dc51c28281b70
yploekito <yp.loekito@gmail.com>
2019-05-16 19:36:28 +0700

    initial

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,17 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCL3zeaR76SBRmq3lMMHBkO09rWAEtPtjo",
+  authDomain: "grabduit.firebaseapp.com",
+  projectId: "grabduit"
+})
+
+export const db = firebase.firestore();
+
 new Vue({
   router,
   store,
---
+++ client/src/views/Rooms.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button>create Room</button>
+    
+  </div>
+</template>
+
+<script>
+import { db } from "@/main.js";
+
+export default {
+  name: "Room",
+  data(){
+      return{
+          rooms:[]
+      }
+  },
+  created() {
+    console.log(db)
+    db
+        .collection("rooms")
+        .onSnapshot(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                this.rooms.push(doc.data())
+            });
+        });
+    }
+};
+</script>
